ad0a850b483098c1ee7ad7a8197d43ce7914cd97
murraycadzow <cadmu661@student.otago.ac.nz>
2024-08-08 15:20:05 +1200

    update util.cpp

---
+++ includes/util.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <iostream>
+
+
+// printf("MAX_PATH: %d\n", MAX_PATH);
+//     TCHAR buffer[MAX_PATH];
+//     DWORD length = GetCurrentDirectory(MAX_PATH, buffer);
+//     if(length == 0){
+//         printf("Error\n");
+//     } else {
+//         printf("Current directory: %s\n", buffer);
+//     }

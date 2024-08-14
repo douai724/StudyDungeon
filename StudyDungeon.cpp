e9de6bc8bbaebb655526a9317571170606041575
Aidan Dougherty <douai724@student.otago.ac.nz>
2024-08-14 14:36:26 +1200

    Menu version 1
    just playing around with the main menu

--- StudyDungeon.cpp
+++ StudyDungeon.cpp
@@ -68,16 +68,33 @@ int main() {
     fs::path deckDir = appDir.append("Decks");
     load_flashcard_decks(deckDir);
 
+    std::string controls = "\n\nControls: Arrow keys to navigate, Enter to select, Esc to go back";
+
     // Menu system
-    auto mainMenu = std::make_shared<Menu>("Flashcard Application");
-    auto editMenu = std::make_shared<Menu>("Edit Flashcards");
+    auto mainMenu = std::make_shared<Menu>("Flashcard Application" + controls);
+    auto editMenu = std::make_shared<Menu>("Edit Flashcards" + controls);
+    auto reviewMenu = std::make_shared<Menu>("Review Flashcards" + controls);
+
+    auto flashcardMenu = std::make_shared<Menu>("Flashcard Menu" + controls);
 
     editMenu->addItem("Delete Flashcard", deleteFlashcard);
     editMenu->addItem("Browse Deck", browseDeck);
     editMenu->addItem("Edit Card", editCard);
+    editMenu->addItem("Back to Main Menu", mainMenu);
+    editMenu->addItem("Exit", exitProgram);
+
+    reviewMenu->addItem("Open Flashcard", flashcardMenu);
+    reviewMenu->addItem("Back to Main Menu", mainMenu);
+
+    flashcardMenu->addItem("Easy", reviewFlashcards);
+    flashcardMenu->addItem("Medium", reviewFlashcards);
+    flashcardMenu->addItem("Hard", reviewFlashcards);
+    flashcardMenu->addItem("Back to Review Menu", reviewMenu);
+    flashcardMenu->addItem("Back to Main Menu", mainMenu);
+    flashcardMenu->addItem("Exit", exitProgram);
 
     mainMenu->addItem("Add Flashcard", addFlashcard);
-    mainMenu->addItem("Review Flashcards", reviewFlashcards);
+    mainMenu->addItem("Review Flashcards", reviewMenu);
     mainMenu->addItem("Edit Flashcards", editMenu);
     mainMenu->addItem("Exit", exitProgram);
 

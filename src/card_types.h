12c6d6a6424f5ac385b74958858709cd2285b2ba
Veronika Luthar <veronika.luthar@gmail.com>
2024-08-23 16:10:36 +1200

    Updated documentation

--- src/card_types.h
+++ src/card_types.h
@@ -1,6 +1,10 @@
 #ifndef CARD_TYPES_H
 #define CARD_TYPES_H
 
+/**
+ * @brief All possible effects that a playing card could have.
+ * 
+ */
 enum Type
 {
     damage,
--- src/playing_card.h
+++ src/playing_card.h
@@ -4,25 +4,79 @@
 #include "card_types.h"
 #include <string>
 
+/**
+ * @brief 
+ * 
+ */
 class PlayingCard
 {
 private:
-    bool target; // 0 for player that possesses this card, 1 for the enemy
+    /**
+     * The target of the card's effect. 0 is for the player that possesses this card.
+     * 1 is for the enemy player.
+     */
+    bool target;
+    /**
+     * The value modifier that goes with the card effect.
+     */
     int value;
 
 public:
+    /**
+     * The effect of the card.
+     */
     enum Type cardType;
 
+    /**
+     * @brief Construct a new Playing Card object.
+     * 
+     * Default Constructor.
+     * 
+     */
     PlayingCard();
 
+    /**
+     * @brief Construct a new Playing Card object.
+     * 
+     * @param target the target of the card
+     * @param cardType the effect of the card
+     * @param value the value modifier of the card
+     */
     PlayingCard(bool target, enum Type cardType, int value);
 
+    /**
+     * @brief Compares another PlayingCard object to this.
+     * 
+     * Comparas another PlayingCard object to this. Returns true if both
+     * PlayingCard objects possess the same member values. Returns false 
+     * otherwise.
+     * 
+     * @param other The card to compare this to.
+     * @return true 
+     * @return false 
+     */
     bool operator==(PlayingCard &other);
 
+    /**
+     * @brief Returns the value of the card.
+     * 
+     * @return int the value of the card.
+     */
     int getValue();
 
+    /**
+     * @brief Returns the target of the card.
+     * 
+     * @return true 
+     * @return false 
+     */
     bool getTarget();
 
+    /**
+     * @brief Converts this PlayingCard object to string.
+     * 
+     * @return std::string the string version of this PlayingCard.
+     */
     std::string toString();
 };
 #endif

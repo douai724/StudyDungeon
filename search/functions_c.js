04e6f7aa5751d827c3a3e153e052a207db856a78
veronika-luthar <cadmu661@student.otago.ac.nz>
2024-08-29 00:40:28 +0000

    veronika-luthar published a site update

--- search/functions_c.js
+++ search/functions_c.js
@@ -4,6 +4,6 @@ var searchData=
   ['sethitpoints_123',['setHitPoints',['../classPlayer.html#a389b9865ee167e58de339e83d16250d0',1,'Player']]],
   ['setmaxhitpoints_124',['setMaxHitPoints',['../classPlayer.html#ad6aeb2075eb5376d9512023e1754be0b',1,'Player']]],
   ['splashscreen_125',['splashScreen',['../artwork_8h.html#a6a9ebf5fc23776639a05c6da2e050f02',1,'artwork.cpp']]],
-  ['start_126',['start',['../gameloop_8h.html#a60de64d75454385b23995437f1d72669',1,'gameloop.cpp']]],
+  ['start_126',['start',['../gameloop_8h.html#a29c24ed84d3e4996ea32efc961c65f84',1,'gameloop.cpp']]],
   ['strtocarddifficulty_127',['strToCardDifficulty',['../deck_8cpp.html#a6e4ad1bdbb9764c9291a9da1872265e8',1,'strToCardDifficulty(const std::string &amp;difficultyStr):&#160;deck.cpp'],['../deck_8h.html#a6e4ad1bdbb9764c9291a9da1872265e8',1,'strToCardDifficulty(const std::string &amp;difficultyStr):&#160;deck.cpp']]]
 ];

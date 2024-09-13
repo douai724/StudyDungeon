828b425f92f49d3485aed155c3024eb4a0c7a66f
murraycadzow <cadmu661@student.otago.ac.nz>
2024-09-13 15:29:06 +1200

    document console window functions

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,8 +1,8 @@
 #include "menu.h"
 #include "util.h"
-#include <iostream>
 #include <algorithm>
 #include <conio.h>
+#include <iostream>
 
 namespace ConsoleUI
 {
@@ -19,8 +19,8 @@ COORD getConsoleWindowSize()
 {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    return { static_cast<short>(csbi.srWindow.Right - csbi.srWindow.Left + 1),
-             static_cast<short>(csbi.srWindow.Bottom - csbi.srWindow.Top + 1) };
+    return {static_cast<short>(csbi.srWindow.Right - csbi.srWindow.Left + 1),
+            static_cast<short>(csbi.srWindow.Bottom - csbi.srWindow.Top + 1)};
 }
 
 // ConsoleWindow implementation
@@ -28,6 +28,7 @@ ConsoleWindow::ConsoleWindow() : m_width(0), m_height(0), m_textBoxCapacity(14)
 {
     updateSize();
 }
+
 
 void ConsoleWindow::updateSize()
 {
@@ -49,18 +50,18 @@ void ConsoleWindow::drawBox(int x, int y, int width, int height)
     drawVerticalLine(x + width - 1, y, height);
 }
 
-void ConsoleWindow::drawHorizontalLine(int x, int y, int length)
+void ConsoleWindow::drawHorizontalLine(int x, int y, int length, char ch)
 {
     setConsoleCursorPosition(x, y);
-    std::cout << std::string(length, '-');
+    std::cout << std::string(length, ch);
 }
 
-void ConsoleWindow::drawVerticalLine(int x, int y, int length)
+void ConsoleWindow::drawVerticalLine(int x, int y, int length, char ch)
 {
     for (int i = 0; i < length; ++i)
     {
         setConsoleCursorPosition(x, y + i);
-        std::cout << '|';
+        std::cout << ch;
     }
 }
 
@@ -70,12 +71,13 @@ void ConsoleWindow::drawCharacter(int x, int y, char ch)
     std::cout << ch;
 }
 
-void ConsoleWindow::drawText(const std::string& text, int x, int y, bool highlight)
+void ConsoleWindow::drawText(const std::string &text, int x, int y, bool highlight)
 {
     setConsoleCursorPosition(x, y);
     if (highlight)
     {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                                FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     }
     std::cout << text;
     if (highlight)
@@ -84,7 +86,7 @@ void ConsoleWindow::drawText(const std::string& text, int x, int y, bool highlig
     }
 }
 
-void ConsoleWindow::drawCenteredText(const std::string& text, int y)
+void ConsoleWindow::drawCenteredText(const std::string &text, int y)
 {
     int x = static_cast<int>((m_width - text.length()) / 2);
     drawText(text, x, y);
@@ -97,10 +99,10 @@ void ConsoleWindow::clear()
 
 COORD ConsoleWindow::getSize() const
 {
-    return { static_cast<SHORT>(m_width), static_cast<SHORT>(m_height) };
+    return {static_cast<SHORT>(m_width), static_cast<SHORT>(m_height)};
 }
 
-void ConsoleWindow::addTextToBox(const std::string& text)
+void ConsoleWindow::addTextToBox(const std::string &text)
 {
     m_textBox.push_back(text);
     if (m_textBox.size() > m_textBoxCapacity)
@@ -119,8 +121,9 @@ void ConsoleWindow::drawTextBox(int x, int y, int width, int height)
 }
 
 // Button implementation
-Button::Button(const std::string& label, std::function<void()> action)
-    : m_label(label), m_action(action) {}
+Button::Button(const std::string &label, std::function<void()> action) : m_label(label), m_action(action)
+{
+}
 
 void Button::draw(int x, int y, bool selected)
 {
@@ -128,7 +131,8 @@ void Button::draw(int x, int y, bool selected)
     setConsoleCursorPosition(x, y);
     if (selected)
     {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                                FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     }
     std::cout << border << "[" << m_label << "]" << border;
     if (selected)
@@ -139,7 +143,8 @@ void Button::draw(int x, int y, bool selected)
 
 void Button::performAction() const
 {
-    if (m_action) m_action();
+    if (m_action)
+        m_action();
 }
 
 int Button::getWidth() const
@@ -147,16 +152,17 @@ int Button::getWidth() const
     return static_cast<int>(m_label.length() + 4);
 }
 
-const std::string& Button::getLabel() const
+const std::string &Button::getLabel() const
 {
     return m_label;
 }
 
 // Menu implementation
-Menu::Menu(bool horizontal)
-    : m_selectedIndex(0), m_horizontal(horizontal) {}
+Menu::Menu(bool horizontal) : m_selectedIndex(0), m_horizontal(horizontal)
+{
+}
 
-void Menu::addButton(const std::string& label, std::function<void()> action)
+void Menu::addButton(const std::string &label, std::function<void()> action)
 {
     m_buttons.emplace_back(label, action);
 }
@@ -182,17 +188,17 @@ void Menu::draw(int x, int y)
 void Menu::handleInput()
 {
     int key = _getch();
-    if (key == 224)  // Arrow key
+    if (key == 224) // Arrow key
     {
         key = _getch();
         if (m_horizontal)
         {
             switch (key)
             {
-            case 75:  // Left arrow
+            case 75: // Left arrow
                 m_selectedIndex = (m_selectedIndex - 1 + m_buttons.size()) % m_buttons.size();
                 break;
-            case 77:  // Right arrow
+            case 77: // Right arrow
                 m_selectedIndex = (m_selectedIndex + 1) % m_buttons.size();
                 break;
             }
@@ -201,16 +207,16 @@ void Menu::handleInput()
         {
             switch (key)
             {
-            case 72:  // Up arrow
+            case 72: // Up arrow
                 m_selectedIndex = (m_selectedIndex - 1 + m_buttons.size()) % m_buttons.size();
                 break;
-            case 80:  // Down arrow
+            case 80: // Down arrow
                 m_selectedIndex = (m_selectedIndex + 1) % m_buttons.size();
                 break;
             }
         }
     }
-    else if (key == 13)  // Enter key
+    else if (key == 13) // Enter key
     {
         m_buttons[m_selectedIndex].performAction();
     }
@@ -239,7 +245,9 @@ void Menu::popPage()
 }
 
 // UIManager implementation
-UIManager::UIManager() : m_window(std::make_shared<ConsoleWindow>()) {}
+UIManager::UIManager() : m_window(std::make_shared<ConsoleWindow>())
+{
+}
 
 std::shared_ptr<ConsoleWindow> UIManager::getWindow()
 {
@@ -275,14 +283,14 @@ void UIManager::handleInput()
     }
 }
 
-Menu& UIManager::createMenu(const std::string& name, bool horizontal)
+Menu &UIManager::createMenu(const std::string &name, bool horizontal)
 {
     return m_menus.emplace(name, Menu(horizontal)).first->second;
 }
 
-Menu& UIManager::getMenu(const std::string& name)
+Menu &UIManager::getMenu(const std::string &name)
 {
     return m_menus.at(name);
 }
 
-} 
+} // namespace ConsoleUI
--- src/menu.h
+++ src/menu.h
@@ -12,99 +12,212 @@
 #ifndef MENU_H
 #define MENU_H
 
-#include <string>
-#include <vector>
 #include <functional>
 #include <memory>
+#include <string>
 #include <unordered_map>
+#include <vector>
 #include <windows.h>
+
+
+/**
+ * @brief UI manager -> ConsoleWindow -> Scenes
+ *
+ */
+
 
 namespace ConsoleUI
 {
-    void setConsoleCursorPosition(int x, int y);
-    COORD getConsoleWindowSize();
+void setConsoleCursorPosition(int x, int y);
+COORD getConsoleWindowSize();
 
-    class ConsoleWindow
-    {
-    public:
-        ConsoleWindow();
-        void updateSize();
-        void drawBorder();
-        void drawBox(int x, int y, int width, int height);
-        void drawHorizontalLine(int x, int y, int length);
-        void drawVerticalLine(int x, int y, int length);
-        void drawCharacter(int x, int y, char ch);
-        void drawText(const std::string& text, int x, int y, bool highlight = false);
-        void drawCenteredText(const std::string& text, int y);
-        void clear();
-        COORD getSize() const;
-        void addTextToBox(const std::string& text);
-        void drawTextBox(int x, int y, int width, int height);
+/**
+     * @brief Sets up and controls the console window
+     *
+     */
+class ConsoleWindow
+{
+public:
+    ConsoleWindow();
+    /**
+         * @brief updates the maximum size of the console window
+         *
+         */
+    void updateSize();
 
-    private:
-        int m_width;
-        int m_height;
-        std::vector<std::string> m_textBox;
-        int m_textBoxCapacity;
-    };
+    /**
+         * @brief Draws a border around the console
+         *
+         */
+    void drawBorder();
 
-    class Button {
-    public:
-        Button(const std::string& label, std::function<void()> action);
-        void draw(int x, int y, bool selected);
-        void performAction() const;
-        int getWidth() const;
-        const std::string& getLabel() const;
+    /**
+         * @brief Draws a box on the console starting from top-left corner
+         *
+         * @param x starting number of characters from left
+         * @param y starting number of lines from top
+         * @param width width of box
+         * @param height height of box
+         */
+    void drawBox(int x, int y, int width, int height);
 
-    private:
-        std::string m_label;
-        std::function<void()> m_action;
-    };
+    /**
+         * @brief Draws a horizontal line of defined length using a character
+         *
+         * @param x starting number of characters from left
+         * @param y starting number of lines from top
+         * @param length length of the line
+         * @param ch character to use for the line
+         */
+    void drawHorizontalLine(int x, int y, int length, char ch = '-');
 
-    class Menu
-    {
-    public:
-        Menu(bool horizontal = false);
-        void addButton(const std::string& label, std::function<void()> action);
-        void draw(int x, int y);
-        void handleInput();
-        bool isBackButtonPressed() const;
-        void pushPage();
-        void popPage();
+    /**
+         * @brief  Draws a vertical line of defined length using a character
+         *
+         * @param x starting number of characters from left
+         * @param y starting number of lines from top
+         * @param length length of the line
+         * @param ch character to use for the line
+         */
+    void drawVerticalLine(int x, int y, int length, char ch = '|');
 
-    private:
-        std::vector<Button> m_buttons;
-        size_t m_selectedIndex;
-        bool m_horizontal;
-        std::vector<std::vector<Button>> m_pageHistory;
-    };
+    /**
+         * @brief Draws a character as a specific coordinate
+         *
+         * @param x number of characters from left
+         * @param y number of lines from top
+         * @param ch Character to print
+         */
+    void drawCharacter(int x, int y, char ch);
 
-    class Scene
-    {
-    public:
-        virtual ~Scene() = default;
-        virtual void update() = 0;
-        virtual void render(std::shared_ptr<ConsoleWindow> window) = 0;
-        virtual void handleInput() = 0;
-    };
+    /**
+         * @brief Prints text to console starting from specific location
+         *
+         * @param text Text to write
+         * @param x number of characters from left
+         * @param y number of lines from top
+         * @param highlight bool to toggle text background on/off
+         */
+    void drawText(const std::string &text, int x, int y, bool highlight = false);
 
-    class UIManager
-    {
-    public:
-        UIManager();
-        std::shared_ptr<ConsoleWindow> getWindow();
-        void setCurrentScene(std::shared_ptr<Scene> scene);
-        void update();
-        void render();
-        void handleInput();
-        Menu& createMenu(const std::string& name, bool horizontal = false);
-        Menu& getMenu(const std::string& name);
+    /**
+         * @brief Centers on X-axis at specific y
+         *
+         * @param text Text to write
+         * @param y number of lines from top (from top)
+         */
+    void drawCenteredText(const std::string &text, int y);
 
-    private:
-        std::shared_ptr<ConsoleWindow> m_window;
-        std::shared_ptr<Scene> m_currentScene;
-        std::unordered_map<std::string, Menu> m_menus;
-    };
-}
+    /**
+         * @brief Clears the screen
+         *
+         */
+    void clear();
+
+    /**
+         * @brief Gets the max width and height of the console
+         *
+         * @return COORD
+         */
+    COORD getSize() const;
+
+    /**
+         * @brief
+         *
+         * @param text
+         */
+    void addTextToBox(const std::string &text);
+
+    /**
+         * @brief
+         *
+         * @param x number of characters from left
+         * @param y number of lines from top
+         * @param width wdith of the box
+         * @param height height of the box
+         */
+    void drawTextBox(int x, int y, int width, int height);
+
+private:
+    /** max width of console */
+    int m_width;
+
+    /** max height of console */
+    int m_height;
+
+    /** vector of strings to be printed in a text box */
+    std::vector<std::string> m_textBox;
+
+    /** maximum lines in textbox */
+    int m_textBoxCapacity;
+};
+
+class Button
+{
+public:
+    Button(const std::string &label, std::function<void()> action);
+    void draw(int x, int y, bool selected);
+    void performAction() const;
+    int getWidth() const;
+    const std::string &getLabel() const;
+
+private:
+    std::string m_label;
+    std::function<void()> m_action;
+};
+
+class Menu
+{
+public:
+    Menu(bool horizontal = false);
+    void addButton(const std::string &label, std::function<void()> action);
+    void draw(int x, int y);
+    void handleInput();
+    bool isBackButtonPressed() const;
+    void pushPage();
+    void popPage();
+
+private:
+    std::vector<Button> m_buttons;
+    size_t m_selectedIndex;
+    bool m_horizontal;
+    std::vector<std::vector<Button>> m_pageHistory;
+};
+
+/**
+     * @brief
+     *
+     */
+class Scene
+{
+public:
+    virtual ~Scene() = default;
+    virtual void update() = 0;
+    virtual void render(std::shared_ptr<ConsoleWindow> window) = 0;
+    virtual void handleInput() = 0;
+};
+
+/**
+     * @brief
+     *
+     */
+class UIManager
+{
+public:
+    UIManager();
+    std::shared_ptr<ConsoleWindow> getWindow();
+    void setCurrentScene(std::shared_ptr<Scene> scene);
+    void update();
+    void render();
+    void handleInput();
+    Menu &createMenu(const std::string &name, bool horizontal = false);
+    Menu &getMenu(const std::string &name);
+
+private:
+    std::shared_ptr<ConsoleWindow> m_window;
+    std::shared_ptr<Scene> m_currentScene;
+    std::unordered_map<std::string, Menu> m_menus;
+};
+} // namespace ConsoleUI
 
 #endif // MENU_H

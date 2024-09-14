47de83e0ce232169a10e2b84f35fecebd1ecdbd8
murraycadzow <murray.cadzow@gmail.com>
2024-09-14 19:43:03 +1200

    add tests for std::out methods in deck

--- tests/deck_test.cpp
+++ tests/deck_test.cpp
@@ -1,6 +1,21 @@
-#include <catch2/catch_test_macros.hpp>
-
 #include "deck.h"
+#include <catch2/catch_test_macros.hpp>
+#include <sstream>
+
+struct cout_redirect
+{
+    cout_redirect(std::streambuf *new_buffer) : old(std::cout.rdbuf(new_buffer))
+    {
+    }
+
+    ~cout_redirect()
+    {
+        std::cout.rdbuf(old);
+    }
+
+private:
+    std::streambuf *old;
+};
 
 TEST_CASE("deck_h enum converters")
 {
@@ -43,6 +58,24 @@ TEST_CASE("FlashCard Creation")
         REQUIRE(fc_test.answer == "answer");
         REQUIRE(fc_test.difficulty == MEDIUM);
         REQUIRE(fc_test.n_times_answered == 1);
+    }
+
+    SECTION("Card methods")
+    {
+        FlashCard fc{};
+        auto stdoutBuffer = std::cout.rdbuf();
+        std::ostringstream oss;
+        std::cout.rdbuf(oss.rdbuf());
+
+        fc.printCard();
+        REQUIRE(oss.str() == "\n\n\n");
+        // clear out the buffer ready to take next output
+        oss.str("");
+        oss.clear();
+
+        fc.printCardAsTemplate();
+        REQUIRE(oss.str() == "Q: \nA: \nD: UNKNOWN\nN: 0\n-\n");
+        std::cout.rdbuf(stdoutBuffer); // reset to original std::cout buffer
     }
 }
 

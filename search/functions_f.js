e7b4f3eb5d5f965eade1369a99690cd3bf0f30bb
murraycadzow <cadmu661@student.otago.ac.nz>
2024-10-02 08:35:43 +0000

    murraycadzow published a site update

--- search/functions_f.js
+++ search/functions_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['time_5felapsed_227',['time_elapsed',['../util_8cpp.html#a17bedbeaade121b1157c8042c00f5da6',1,'time_elapsed(const std::chrono::time_point&lt; std::chrono::steady_clock &gt; &amp;start_time, int &amp;duration_secs):&#160;util.cpp'],['../util_8h.html#a17bedbeaade121b1157c8042c00f5da6',1,'time_elapsed(const std::chrono::time_point&lt; std::chrono::steady_clock &gt; &amp;start_time, int &amp;duration_secs):&#160;util.cpp']]],
+  ['timecomplete_227',['timeComplete',['../util_8cpp.html#ab897fdb041fb5dd063d5b54dbccb00a0',1,'timeComplete(const std::chrono::time_point&lt; std::chrono::steady_clock &gt; &amp;start_time, const int &amp;duration_secs):&#160;util.cpp'],['../util_8h.html#ab897fdb041fb5dd063d5b54dbccb00a0',1,'timeComplete(const std::chrono::time_point&lt; std::chrono::steady_clock &gt; &amp;start_time, const int &amp;duration_secs):&#160;util.cpp']]],
   ['tostring_228',['toString',['../classPlayer.html#af26b9335d328ead7f0912b2cb38340bb',1,'Player::toString()'],['../classPlayingCard.html#aea06e402994ae2a69bf11529a62a31c9',1,'PlayingCard::toString()']]]
 ];
